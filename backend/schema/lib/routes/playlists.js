9793650488e351ba1bd74aa2505401afc20aeaa2
pfp2026test <thaabit2012@gmail.com>
2026-08-15 12:37:43 +0100

    Update playlists.js

--- backend/schema/lib/routes/playlists.js
+++ backend/schema/lib/routes/playlists.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { z } from "zod";
-import prisma from "../lib/prisma.js";
+import prisma from "../prisma.js";
 import { requireAdmin } from "../middleware/requireAdmin.js";
 
 const router = Router();
